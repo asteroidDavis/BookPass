31914fb313230c4fb8e22627bddfdd2c6ac8cb0b
NateDavis <natedavis174@gmail.com>
2017-07-15 15:23:47 -0400

    added a long book to world

--- BookPassDesign/classes/Reader.py
+++ BookPassDesign/classes/Reader.py
@@ -1,9 +1,11 @@
-import Book
-import User
+from Book import Book
+from User import User
+
 
 # a data structure representing a user of BookPass
 class Reader(User):
 
     def __init__(self):
+        User.__init__(self)
         # pick default value so the db can be queried in one go
         self.books = []
--- BookPassDesign/tests/world.py
+++ BookPassDesign/tests/world.py
@@ -1,10 +1,11 @@
 from BookPassDesign.classes import Book
-
+from lettuce import world
 
 '''
 a_long_book() - return an object of the book class with hundreds of pages and fields
 '''
 #todo: finish this then finish the rest of entries in tests\Page.py
+@world.absorb
 def a_long_book():
     long_book = Book.Book()
     return long_book
